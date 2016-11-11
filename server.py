5f0741217c8fb15bd0f56d9740b5f9f7c27f5833
Michael Phillips <github@idiotandrobot.com>
2016-11-11 10:04:11 +0000

    Fix server start_service

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ import os
 import signal
 import subprocess
 
+FILENAME = 'heathergraph.py'
+SERVICENAME = 'Heathergraph'
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('operation',
@@ -30,35 +33,35 @@ def get_absolute_filename(relative_name):
     return os.path.abspath(os.path.join(os.path.dirname(__file__),
                                         relative_name))
 
-def start_service():
+def start_service(launch_command):
     proc = subprocess.Popen(launch_command)
-    print('Heathergraph service started - PID={}'.format(proc.pid))
+    print SERVICENAME, 'service started - PID={}'.format(proc.pid)
 
 def stop_service(pid):
     if pid:
-        print('Stopping Heathergraph service - PID={}'.format(pid))
+        print 'Stopping %s service - PID={}'.format(pid) % SERVICENAME
         os.kill(pid, signal.SIGTERM)            
     else:
-        print('No Heathergraph service was found to be running')
+        print 'No %s service was found to be running' % SERVICENAME
     
 
 def main():
     args = parse_args()
     launch_command = ['python',
-                      get_absolute_filename('heathergraph.py')]
+                      get_absolute_filename(FILENAME)]
     pid = find_server_process(launch_command)
 
     if args.operation == 'start':
         if not pid:
-            start_service()
+            start_service(launch_command)
         else:
-            print('Heathergraph service is already running')
+            print SERVICENAME, 'service is already running'
 
     elif args.operation == 'stop':
         stop_service(pid)
     elif args.operation == 'restart':
         stop_service(pid)
-        start_service()
+        start_service(launch_command)
 
 if __name__ == '__main__':
     main()
