9dad4f997371011ee7fe9f6ecd0c1a58cbba6d27
Michael Phillips <github@idiotandrobot.com>
2020-06-28 14:56:51 +0100

    Add support for html2text or no parser Will still prefer beautifulsoup4 if installed

--- html_parse.py
+++ html_parse.py
@@ -1,5 +1,31 @@
-from bs4 import BeautifulSoup
+import imp
+import logging
+log = logging.getLogger(__name__)
 
-def parse(html):
-    soup = BeautifulSoup(html, features="html.parser")
-    return soup.get_text()
+def module_exists(module_name):
+    try:
+        imp.find_module(module_name)
+        return True
+    except ImportError:
+        return False
+
+if module_exists("bs4"):    
+    log.info("Parsing HTML using beautifulsoup4")
+    from bs4 import BeautifulSoup
+
+    def parse(html):
+        soup = BeautifulSoup(html, features="html.parser")
+        return soup.get_text()
+elif module_exists("html2text"):    
+    log.info("Parsing HTML using html2text")
+    import html2text
+
+    def parse(html):
+        h = html2text.HTML2Text()
+        h.single_line_break = True
+        return h.handle(html)
+else:
+    warning_msg = "HTML parsing not available. Install beautifulsoup4 or html2text"
+    log.warning(warning_msg)
+    def parse(html):
+        raise ImportWarning(warning_msg)
